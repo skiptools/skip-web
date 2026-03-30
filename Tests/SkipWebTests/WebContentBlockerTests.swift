dca63c1519c5b1ac578d44cc80938e4e30c0a71e
tifroz <hhardel@gmail.com>
2026-03-29 17:38:51 -0700

    Fix for the gh CI Action's Copyright / License checks

--- Tests/SkipWebTests/WebContentBlockerTests.swift
+++ Tests/SkipWebTests/WebContentBlockerTests.swift
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
+// Copyright 2023-2026 Skip
+// SPDX-License-Identifier: MPL-2.0
 import XCTest
 import Foundation
 #if !SKIP
